cae34d7bcbe9f9b30eb4ae42357c92c152efd203
Yohannes Sheta <Yohannes Sheta>
2023-09-08 14:46:34 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print-tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - point of Entry
+ *Describtion: print lowor case alphabet exclude unwanted
+ *Return: always 0
+ */
+
+int main(void)
+{
+        int n = 12;
+
+	while (n => 97)
+        {
+                putchar(n);
+		n--
+        }
+        putchar('\n');
+        return (0);
+}
