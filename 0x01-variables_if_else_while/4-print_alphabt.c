d59b490fc232676b4db9d9a9ab2490e8e42b53e5
Yohannes Sheta <Yohannes Sheta>
2023-09-08 13:48:20 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- *Main - point of Entery
- *Descrption: print alphabet and exclude unwant one.
- *return alwaye
+ *main - point of Entry
+ *Describtion: print lowor case alphabet exclude unwanted
+ *Return: always 0
  */
+
 int main(void)
 {
 	int n = 97;
 
-	while (n == 122)
+	while (n <= 122)
 	{
 		if (n == 101 || n == 113)
 		{
