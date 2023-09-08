5a914e8829adcd743c00255b4b425d818fdbe02d
Yohannes Sheta <Yohannes Sheta>
2023-09-08 14:25:38 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - point of Entry
+ *Describtion: print lowor case alphabet exclude unwanted
+ *Return: always 0
+ */
+
+int main(void)
+{
+        int n;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
