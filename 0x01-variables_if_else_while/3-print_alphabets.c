cb90cbc74e75f95fd94720dbc064c357febd2a7d
Yohannes Sheta <Yohannes Sheta>
2023-09-08 13:14:26 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,12 @@
  *Describtion: print lowor case alphabet and then foolw uppercase
  *Return: always 0
  */
-int main (void)
+
+int main(void)
 {
 	int n = 97;
-	int m =65;
-	
+	int m = 65;
+
 	while (n <= 122)
 	{
 		putchar(n);
