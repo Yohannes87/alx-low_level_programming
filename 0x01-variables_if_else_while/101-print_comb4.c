7ba8f280687b4f53021dcf7b218da1759796a178
Yohannes Sheta <Yohannes Sheta>
2023-09-10 04:46:18 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,29 +4,28 @@
  *Descrription: combination of three number
  *Return: Always 0
  */
-
 int main(void)
 {
 	int a, b, c;
 
 	for (a = 48; a <= 57; a++)
 	{
-	for (b = a + 1; b <= 57; b++)
-	{
-	for (c = b + 1; c <= 57; c++)
-	{
-	if ((b != a) != c)
-	{
-		putchar(a);
-putchar(b);
-putchar(c);
-if (a == 55 && b == 56)
-continue;
-putchar(',');
-putchar(' ');
-	}
-	}
-	}
+		for (b = a + 1; b <= 57; b++)
+		{
+			for (c = b + 1; c <= 57; c++)
+			{
+				if ((b != a) != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == 55 && b == 56)
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
