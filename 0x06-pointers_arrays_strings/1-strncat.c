9fe2ea436b33d36ba0515d34f493b7c0ad2b9a94
Yohannes Sheta <Yohannes Sheta>
2023-09-20 09:31:56 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strncat - concatentes two string
+ *@dest: destination file
+ *@src: source file
+ *@n: input number
+ *Returb: return to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	if (j < n)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
